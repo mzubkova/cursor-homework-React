a3ea47d5379a129022360cdb6897264ca40f5100
mzubkova <59561550+mzubkova@users.noreply.github.com>
2020-09-26 17:07:31 +0300

    Updates

---
+++ precache-manifest.307907da244c390600c7d8cf75766141.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cc75463b1ea9af14ec469dae08823b",
+    "url": "/cursor-homework-react/index.html"
+  },
+  {
+    "revision": "0de0bddceca688e5e899",
+    "url": "/cursor-homework-react/static/css/main.028560b4.chunk.css"
+  },
+  {
+    "revision": "91e0a41837203989c54d",
+    "url": "/cursor-homework-react/static/js/2.d8913e75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-homework-react/static/js/2.d8913e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de0bddceca688e5e899",
+    "url": "/cursor-homework-react/static/js/main.074e97c8.chunk.js"
+  },
+  {
+    "revision": "1e7b53cccb42f1ec3e10",
+    "url": "/cursor-homework-react/static/js/runtime-main.7899054d.js"
+  },
+  {
+    "revision": "880ac01e1a0fb4fa713fb5e9c96bd180",
+    "url": "/cursor-homework-react/static/media/comment.880ac01e.svg"
+  },
+  {
+    "revision": "c27a63e2057fe7f1574bb53e4ba153ef",
+    "url": "/cursor-homework-react/static/media/correct.c27a63e2.svg"
+  },
+  {
+    "revision": "76a9b341d634058c7f5a7c52ccbda75b",
+    "url": "/cursor-homework-react/static/media/download.76a9b341.svg"
+  },
+  {
+    "revision": "c7e469f73311585c656fc6047340df84",
+    "url": "/cursor-homework-react/static/media/heart.c7e469f7.svg"
+  },
+  {
+    "revision": "8392a35d25ad5d6d4f96cdb3daf75923",
+    "url": "/cursor-homework-react/static/media/retweet.8392a35d.svg"
+  }
+]);
